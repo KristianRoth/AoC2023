20404472b1dafae224d552f93f0dffe31c3069e0
Kristian Roth <kristian@identio.fi>
2023-12-08 17:02:03 +0200

    README update

--- generateRM.py
+++ generateRM.py
@@ -19,8 +19,8 @@ content.append('| --- | --- | --- | --- | --- |')
 for i,dir in enumerate(dirs):
     fileName = dir + '/' + dir.lower() + '.c'
     time = subprocess.check_output([f'./aoc', f'{i+1}', '100'])
-    time = '{:.2f} us'.format(float(time)*1000_000)
-    content.append(f'| [{emojis[i]}](https://adventofcode.com/2023/day/{dir.replace("AoC","")}) | [{dir.  replace("aoc", "Day ").capitalize()}]({fileName}) | {time} | ✅ | ✅ |')
+    time = '{:g}'.format(float('{:.{p}g}'.format(float(time)*1000_000, p=2)))
+    content.append(f'| [{emojis[i]}](https://adventofcode.com/2023/day/{dir.replace("AoC","")}) | [{dir.  replace("aoc", "Day ").capitalize()}]({fileName}) | {time} μs | ✅ | ✅ |')
 
 with open('README.md', 'w') as file:
     file.write('\n'.join(content))
