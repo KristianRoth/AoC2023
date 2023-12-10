fcedb58e215c25cf6b559e5588f6565d358815af
Kristian Roth <kristian@identio.fi>
2023-12-10 12:39:51 +0200

    Optimized day9

--- AoC9/aoc9.c
+++ AoC9/aoc9.c
@@ -8,77 +8,33 @@
 #include <math.h>
 #include <limits.h>
 
-void parse_analysis(char *line, IntNode **firsts_ref, IntNode **lasts_ref) {
-    Analysis *nums = NULL;
-    IntNode *firsts;
-    IntNode *lasts = NULL;
+Analysis *parse_analysis(char *line) {
+    Analysis *nums = malloc(sizeof(Analysis));
+    int count = 0;
     while (*line != '\0') {
-        Analysis *new = malloc(sizeof(Analysis));
-        new->value = Common_get_number_lstrip(&line);
-        if (lasts == NULL) {
-            lasts = malloc(sizeof(IntNode));
-            lasts->value = new->value;
-        }
-        new->next  = nums;
-        nums = new;
+        nums->values[ana_index(0, count++)] = Common_get_number_lstrip(&line);
     }
-    firsts = malloc(sizeof(IntNode));
-    firsts->value = nums->value;
-    Analysis *prev = nums;
-    while (true) {
-        bool only_zeros = true;
-        Analysis *layer = NULL;
-        Analysis *prev_iter = prev;
-        int last_value;
-        while (prev_iter->next != NULL) {
-            Analysis *new = malloc(sizeof(Analysis));
-            new->value = prev_iter->value - prev_iter->next->value;
-            if (new->value != 0) only_zeros = false;
-            if (layer == NULL) {
-                IntNode *new_first = malloc(sizeof(IntNode));
-                new_first->next = firsts;
-                new_first->value = new->value;
-                firsts = new_first;
-                layer = new;
-            } else {
-                Analysis *tmp = layer;
-                while (tmp->next != NULL) tmp = tmp->next;
-                tmp->next = new;
-            }
-            last_value = new->value;
-            prev_iter = prev_iter->next;
+    for (int k = 1; k < ANA_NUM; k++) {
+        for (int n = 0; n < ANA_NUM-k; n++) {
+            nums->values[ana_index(k, n)] = nums->values[ana_index(k-1, n+1)] - nums->values[ana_index(k-1, n)];
         }
-        IntNode *new_last = malloc(sizeof(IntNode));
-        new_last->next = lasts;
-        new_last->value = last_value;
-        lasts = new_last;
-        layer->layer = prev;
-        if (only_zeros) {
-            break;
-        }
-        prev = layer;
     }
-    *firsts_ref = firsts;
-    *lasts_ref = lasts;
+    return nums;
 }
 
-int calculate_next(IntNode *a) {
-    int sum = a->value;
-    int count = 0;
-    while (a->next) {
-        sum = a->next->value + sum;
-        a = a->next;
-    };
+int calculate_next(Analysis *a) {
+    int sum = 0;
+    for (int i = 0; i < ANA_NUM; i++) {
+        sum += a->values[ana_index(i, ANA_NUM-i-1)];
+    }
     return sum;
 }
 
-int calculate_prev(IntNode *a) {
-    int sum = a->value;
-    int count = 0;
-    while (a->next) {
-        sum = a->next->value - sum;
-        a = a->next;
-    };
+int calculate_prev(Analysis *a) {
+    int sum = 0;
+    for (int i = ANA_NUM-1; i >= 0; i--) {
+        sum = a->values[ana_index(i, 0)] - sum;
+    }
     return sum;
 }
 
@@ -87,11 +43,9 @@ void Day9_solve() {
     int sum = 0;
     int sum_lasts = 0;
     do {
-        IntNode *firsts, *lasts;
-        parse_analysis(head->value, &firsts, &lasts);
-        sum += calculate_next(firsts);
-        sum_lasts += calculate_prev(lasts);
-
+        Analysis *a = parse_analysis(head->value);
+        sum += calculate_next(a);
+        sum_lasts += calculate_prev(a);
     } while(head = head->next);
     printf("Answer 1st: %i\n", sum);
     printf("Answer 2nd: %i\n", sum_lasts);
--- AoC9/aoc9.h
+++ AoC9/aoc9.h
@@ -1,11 +1,12 @@
 #ifndef AOC9_H
 #define AOC9_H
 
+#define ANA_NUM 21
+#define ana_index(k, n) ((k)*ANA_NUM+(n))
+
 typedef struct Analysis Analysis;
 struct Analysis {
-    int value;
-    Analysis *next;
-    Analysis *layer;
+    int values[ANA_NUM*ANA_NUM];
 };
 
 void Day9_solve(void);
