aa0188aabc44ef81af128ee82042041454be84f4
Kristian Roth <kristian@identio.fi>
2023-12-11 17:27:59 +0200

    Optimized Day11

--- AoC11/aoc11.c
+++ AoC11/aoc11.c
@@ -5,89 +5,50 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void insert_star(StarChart **c_ref, int x, int y, int no_stars_count) {
-    StarChart *c = *c_ref;
-    StarChart *new = malloc(sizeof(StarChart));
-    new->x = x;
-    new->y = y + no_stars_count;
-    new->x_l = x;
-    new->y_l = y + 999999*no_stars_count;
-    if (c == NULL || c->x >= x) {
-        new->next = c;
-        c = new;
-    } else {
-        StarChart *iter = c;
-        while (iter->next != NULL && iter->next->x < x) iter = iter->next;
-        new->next = iter->next;
-        iter->next = new;
-    }
-    *c_ref = c;
-}
+void Day11_solve() {
+    Node *head = Common_readFile("AoC11/day11.txt");
+    int stars_x[STARS];
+    int stars_y[STARS];
+    memset(stars_x, 0, sizeof(stars_x));
+    memset(stars_y, 0, sizeof(stars_y));
 
-StarChart *parse_stars(Node *head) {
-    StarChart *c = NULL;
     int count_y = 0;
     int no_stars_count = 0;
     do {
         char *line = head->value;
         int count_x = 0;
-        bool no_stars = true;
         while (*line != '\0') {
             if (*line == '#') {
-                no_stars = false;
-                insert_star(&c, count_x, count_y, no_stars_count);
+                stars_x[count_x]++;
+                stars_y[count_y]++;
             }
             line++;
             count_x++;
         }
-        no_stars_count += no_stars;
         count_y++;
     } while (head = head -> next);
 
-    StarChart *iter = c;
-    int empty_xs = 0;
-    int index = 0;
-    while (iter != NULL) {
-        if (iter->x != index) {
-            empty_xs++;
-            index++;
-            continue;
-        }
-        do {
-            iter->x += empty_xs;
-            iter->x_l += 999999*empty_xs;
-            iter = iter->next;
-        } while (iter != NULL && iter->x == index);
-        index++;
+    int offset_x[STARS];
+    int offset_y[STARS];
+    offset_x[0] = 0;
+    offset_y[0] = 0;
+    for (int i = 1; i < STARS; i++) {
+        offset_x[i] = (stars_x[i] == 0 ? 1 : 0) + offset_x[i-1];
+        offset_y[i] = (stars_y[i] == 0 ? 1 : 0) + offset_y[i-1];
     }
-    return c;
-}
 
-void Day11_solve() {
-    Node *head = Common_readFile("AoC11/day11.txt");
-    StarChart *c = parse_stars(head);
     long sum = 0;
-    long sum_l = 0;
-    do {
-        StarChart *iter = c->next;
-        while (iter) {
-            sum += iter->x - c->x + abs(iter->y - c->y);
-            sum_l += iter->x_l - c->x_l + abs(iter->y_l - c->y_l);
-            iter = iter->next;
+    long sum2 = 0;
+    for (int i = 0; i < STARS; i++) {
+        for (int j = i+1; j < STARS; j++) {
+            sum +=  (stars_x[i]*stars_x[j])*(j - i + (offset_x[j] - offset_x[i]));
+            sum +=  (stars_y[i]*stars_y[j])*(j - i + (offset_y[j] - offset_y[i]));
+
+            sum2 += (stars_x[i]*stars_x[j])*(j - i + (offset_x[j] - offset_x[i])*999999);
+            sum2 += (stars_y[i]*stars_y[j])*(j - i + (offset_y[j] - offset_y[i])*999999);
+
         }
-    } while(c = c->next);
-
+    }
     printf("Answer 1st: %li\n", sum);
-    printf("Answer 2nd: %li\n", sum_l);
+    printf("Answer 2nd: %li\n", sum2);
 }
-
-
-// 833166416
-// 832678904
-// 832324192
-// 832323778
-// 635412704
-
-// 842645913794
-
-
--- AoC11/aoc11.h
+++ AoC11/aoc11.h
@@ -1,15 +1,7 @@
 #ifndef AOC11_H
 #define AOC11_H
 
-
-typedef struct StarChart StarChart;
-struct StarChart {
-    int x;
-    int y;
-    int x_l;
-    int y_l;
-    StarChart *next;
-};
+#define STARS 140
 
 void Day11_solve(void);
 
